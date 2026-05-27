1359dc90b1825b3d6ba7187f7ffa544ba30363ea
r7ee0 <reeejus122@gmail.com>
2026-05-27 20:06:24 +0545

    new

---
+++ nested.sql
@@ -0,0 +1,12 @@
+SELECT Lecturer_id
+FROM course_lecturers
+GROUP BY Lecturer_id
+HAVING COUNT(Course_id) = 1;
+
+
+SELECT Lecturer_id
+FROM lecturers
+WHERE Lecturer_id NOT IN (
+    SELECT Lecturer_id
+    FROM course_lecturers
+);
